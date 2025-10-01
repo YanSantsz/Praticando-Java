dfcfb45d5364e67ce041c04ed6078ca080ce9b34
Yan Victor Valério dos Santos Silva <yansantos.silva7@gmail.com>
2025-10-01 16:33:34 -0300

    Atualização do Exercicio

--- src/ForWhile/ContagemDegraus.java
+++ src/ForWhile/ContagemDegraus.java
@@ -1,4 +1,23 @@
 package ForWhile;
 
+import java.util.Scanner;
+
 public class ContagemDegraus {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int degraus = 0;
+
+        System.out.print("Digite a quantidade de degraus: ");
+        degraus = sc.nextInt();
+
+        for (int i = 1; i <= degraus; i++) {
+            System.out.println("Subindo o degrau " + i);
+        }
+
+        System.out.println("Você chegou ao topo!");
+
+        sc.close();
+
+    }
 }
