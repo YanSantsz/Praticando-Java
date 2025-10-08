639992ccdbe1f90810ffb325dbc81d997a5f6979
Yan Victor Valério dos Santos Silva <yansantos.silva7@gmail.com>
2025-10-08 11:27:27 -0300

    Erro no envio, enviando nova atividade

--- src/ForWhile/NumeroMaior.java
+++ src/ForWhile/NumeroMaior.java
@@ -1,4 +1,27 @@
 package ForWhile;
 
+import java.util.Scanner;
+
 public class NumeroMaior {
-}
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            System.out.print("Digite os números separados por espaço: ");
+            String entrada = sc.nextLine();
+
+            String[] numerosTexto = entrada.split(" ");
+
+            int maior = Integer.parseInt(numerosTexto[0]);
+
+            for (int i = 1; i < numerosTexto.length; i++) {
+                int numeroAtual = Integer.parseInt(numerosTexto[i]);
+
+                if (numeroAtual > maior) {
+                    maior = numeroAtual;
+                }
+            }
+
+            System.out.println("O maior número é: " + maior);
+            sc.close();
+        }
+    }
--- src/ForWhile/TentativasLogin.java
+++ src/ForWhile/TentativasLogin.java
@@ -1,4 +1,28 @@
 package ForWhile;
 
+import java.util.Scanner;
+
 public class TentativasLogin {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int senha;
+        boolean acessoPermitido = false;
+
+        for (int i = 2; i >= 0; i--) {
+            System.out.print("Digite sua senha: ");
+            senha = sc.nextInt();
+            if (senha == 1234) {
+                System.out.println("Senha Correta! Acesso concedido!");
+                acessoPermitido = true;
+                break;
+            } else if (i > 0) {
+                System.out.println("Senha incorreta. Você tem " + i + " tentativas restantes.");
+            }
+        }
+
+        if (!acessoPermitido) {
+            System.out.println("Conta bloqueada temporariamente.");
+        }
+        sc.close();
+    }
 }
